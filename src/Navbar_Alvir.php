0f2f6ea2ced116050f42cb97aae371cd36d76119
AlvirElWapo <correodeandresalvir@gmail.com>
2023-01-06 10:46:28 -0600

    cambios en el estilo primer aviso

---
+++ src/Navbar_Alvir.php
@@ -0,0 +1,5 @@
+<div class="navbar_contenedor">
+
+<h1>HOLA HOLA ESTA ES MI NAVBAR</h1>
+
+</div>
--- src/index.php
+++ src/index.php
@@ -1,3 +1,4 @@
 <?php include 'html_start.php';?>
+<?php include 'Navbar_Alvir.php';?>
 <h1>HOLA TERE ESTO ES PHP</h1>
 <?php include 'html_end.php';?>
