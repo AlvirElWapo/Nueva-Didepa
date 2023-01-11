5de4904daad1c1963d4f305e3fed0e53e81af534
AlvirElWapo <correodeandresalvir@gmail.com>
2023-01-11 11:15:50 -0600

    creación de primer sección, header UAEMEX mejorado

--- src/Navbar_Alvir.php
+++ src/Navbar_Alvir.php
@@ -5,14 +5,14 @@
     <img src="../style/Images/Instagram_icon.png" class="navbar_item">
     <img src="../style/Images/Tiktok_icon.png" class="navbar_item">
 
-    <div class="navbar_item" style="object-fit:contain;width:fit-content;">
+    <div class="special_navbar_item" style="object-fit:contain;width:fit-content;">
         <div style="text-align: center;"><img src="../style/Images/Email_icon.png" style="height: 3vh;"></div>
         <span style="color: white;">rectoria@uaemex.mx</span>
     </div>
 
-    <div class="navbar_item" style="object-fit:contain;width:fit-content;">
-        <div  style="text-align: center;"><img src="../style/Images/Buzon_icon.png" style="height: 3vh;"></div>
-        <span style="color: white;">Buzón de Atención Universitaria</span>
+    <div class="special_navbar_item" data-legend="Buzon de Atención Universitaria" style="object-fit:contain;width:fit-content;">
+        <div  style="text-align:center;"><img src="../style/Images/Buzon_icon.png" style="height: 6vh;"></div>
+        <!-- <span style="color: white;">Buzón de Atención Universitaria</span> -->
     </div>
     
 </div>
