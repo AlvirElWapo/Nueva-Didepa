ff4532dc2a656e6772c4316b4db8d395e09bd673
AlvirElWapo <correodeandresalvir@gmail.com>
2023-01-11 00:07:56 -0600

    Alvir realizando algunos cambios

--- src/index.php
+++ src/index.php
@@ -1,4 +1,14 @@
-<?php include 'html_start.php';?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Didepa</title>
+    <link rel="stylesheet" href="../style/styles.css">
+</head>
+<body>
 <?php include 'Navbar_Alvir.php';?>
 <h1>HOLA TERE ESTO ES PHP</h1>
-<?php include 'html_end.php';?>
+</body>
+</html>
