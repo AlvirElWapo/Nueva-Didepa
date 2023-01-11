5b9fc1d6a1e15520290538f019c05dd4669206e0
AlvirElWapo <correodeandresalvir@gmail.com>
2023-01-11 13:55:30 -0600

    ADICIÓN DEL CONTENEDOR DEL LOGO UAEMEX

--- src/Navbar_Alvir.php
+++ src/Navbar_Alvir.php
@@ -4,15 +4,12 @@
     <img src="../style/Images/youtube_icon.png" class="navbar_item">
     <img src="../style/Images/Instagram_icon.png" class="navbar_item">
     <img src="../style/Images/Tiktok_icon.png" class="navbar_item">
-
-    <div class="special_navbar_item" style="object-fit:contain;width:fit-content;">
-        <div style="text-align: center;"><img src="../style/Images/Email_icon.png" style="height: 3vh;"></div>
-        <span style="color: white;">rectoria@uaemex.mx</span>
-    </div>
-
-    <div class="special_navbar_item" data-legend="Buzon de Atención Universitaria" style="object-fit:contain;width:fit-content;">
-        <div  style="text-align:center;"><img src="../style/Images/Buzon_icon.png" style="height: 6vh;"></div>
-        <!-- <span style="color: white;">Buzón de Atención Universitaria</span> -->
-    </div>
     
+    <div class="special_navbar_item" data-legend="rectoria@uaemex.mx" style="object-fit:contain;width:fit-content;">
+        <div style="text-align: center;"><img src="../style/Images/Email_icon.png"></div>
+    </div>
+
+    <a href="https://atencion.uaemex.mx/"class="special_navbar_item" data-legend="Buzon de Atención Universitaria" style="object-fit:contain;width:fit-content;">
+            <div  style="text-align:center;"><img src="../style/Images/Buzon_icon.png" ></div>
+    </a>
 </div>
--- src/index.php
+++ src/index.php
@@ -4,11 +4,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Didepa</title>
+    <title>Nueva Didepa</title>
     <link rel="stylesheet" href="../style/styles.css">
 </head>
 <body>
+
+
 <?php include 'Navbar_Alvir.php';?>
-<h1>HOLA TERE ESTO ES PHP</h1>
+<?php include 'Logo_UAEM.php';?>
+
+
 </body>
 </html>
